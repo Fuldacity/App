6d5df5318fff388ef4847c02bea470fc19eeaf45
knowledge91 <dirkhornung91@gmail.com>
2018-04-23 19:15:31 +0200

    added events, added tabbar icons

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import News from './news'
-import { TabNavigator } from 'react-navigation'
+import Events from './events.js'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { TabNavigator, TabBarBottom } from 'react-navigation'
 
 export default TabNavigator({
-  Home: { screen: News },
+  News: { screen: News },
+  Events: { screen: Events }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Events') {
+        iconName = `ios-musical-notes${focused ? '' : '-outline'}`;
+      } else if (routeName === 'News') {
+        iconName = `ios-paper${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+    },
+  }),
 })
---
+++ events.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export default class Events extends Component {
+    render() {
+        return (
+            <Text>Events</Text>
+        )
+    }
+}
